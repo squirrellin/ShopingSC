812b921d228fc88861c425f60783adad642b38e6
leishaolin <http://gitlab.cttyun.cn/greatedu/market/backend.git>
2021-03-23 20:22:03 +0800

    第一次提交

---
+++ src/test/java/com/shaolin/shopingsc/ShopingscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaolin.shopingsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopingscApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
